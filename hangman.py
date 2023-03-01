76a7f5b44f097bea9dcbf03c7e374c65672ff914
GoshaKuzovkov0707 <124372454+GoshaKuzovkov0707@users.noreply.github.com>
2023-03-01 20:14:44 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -11,11 +11,20 @@ def game():
     while progress:
         user_guess = input('Введите букву - ')
         template = build_template(template, word_in_play, user_guess)
-        guessed = list_to_spring_convert[template]
+        guessed = list_to_spring_convert(template)
         print(f'Результат {guessed}')
         progress = check_win(guessed)
 
-        
+        if not check_mistake(word_in_play, user_guess):
+            lifes -= 1 
+            print(f'У вас осталось {lifes} жизней')
+
+        if lifes == 0:
+            print('Вы проиграли!')
+            break
+
+        if not progress:
+            print('!!!Вы победили!!!')
 
 def check_win(g):
     if '_' in g:
@@ -24,17 +33,17 @@ def check_win(g):
 
 def welcome_speech (t):
     print(f'''Добро пожаловать в hangman 1.0!
-    Загаданное слово состоит из (len(t)) букв [t]
+    Загаданное слово состоит из {len(t)} букв {t}
     ''')
 
 def start_template(w):
     t = []
     for f in w:
-        t.append
+        t.append('_')
     return t
 
 def list_to_spring_convert(t):
-    s = ' '
+    s = ''
     return s.join(t)
 
 def get_word(w):
@@ -48,3 +57,10 @@ def build_template(t, w, g=''):
             else:
                 t[a] = '_'
     return t
+
+def check_mistake(w, g):
+    if g not in w:
+        return False
+    return True
+
+game()
